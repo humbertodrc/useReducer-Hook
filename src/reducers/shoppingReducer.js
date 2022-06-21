deea09127e22c02dee7fc07ed376390c855be52a
danieldamian09 <humbertorivero9@gmail.com>
2022-06-21 14:52:16 -0300

    Feat validate product in cart

--- src/reducers/shoppingReducer.js
+++ src/reducers/shoppingReducer.js
@@ -1,37 +1,49 @@
-import { TYPES } from '../actions/shoppingActions';
+import {TYPES} from "../actions/shoppingActions";
 import {products} from "../data/products";
-
-
 
 // Estado inicial del carrito de compras
 export const initialState = {
-  cart: [],
-}
+	cart: [],
+};
 
 export const shoppingReducer = (state = initialState, action) => {
-  switch (action.type) {
+	switch (action.type) {
     case TYPES.ADD_TO_CART: {
-      let newItem = products.find(item => item.id === action.payload);
-      return {
-        ...state,
-        cart: [...state.cart, state.cart.map(item => item.id === newItem.id ? newItem : item)]
-      }
-    }
-    
-    case TYPES.REMOVE_ONE_FROM_CART: {
-      return
-    }
-      
-    case TYPES.REMOVE_ALL_FROM_CART: {
-      return
-    }
-      
-    case TYPES.CLEAR_CART: {
-      return
-    }
-  
-    default:
-      return state;
-  }
-}
+      // Buscamos el producto nuevo para agregarlo al carrito
+      let newItem = products.find((item) => item.id === action.payload);
+      // Validamos si ese producto nuevo ya esta en el carrito
+			let itemInCartValidate = state.cart.find(
+				(item) => item.id === newItem.id
+			);
 
+      // Si el producto nuevo ya esta en el carrito, hacemos el condicional del return
+			return itemInCartValidate
+				? {
+						...state,
+						cart: state.cart.map((item) =>
+							item.id === newItem.id
+								? {...item, quantity: item.quantity + 1}
+								: item
+						), // Si el item ya está en el carrito, se suma 1 a la cantidad
+				  }
+				: {
+						...state,
+						cart: [...state.cart, {...newItem, quantity: 1}], //la primera vez que lo agrego le creo la propeidad quantity: 1
+				  };
+		}
+		case TYPES.REMOVE_ONE_FROM_CART: {
+			return;
+		}
+
+		case TYPES.REMOVE_ALL_FROM_CART: {
+			return;
+		}
+
+		case TYPES.CLEAR_CART: {
+			return;
+		}
+
+		default:
+			return state;
+	}
+};
