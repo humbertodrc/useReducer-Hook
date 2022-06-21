c569a6b39a8ea176351c3b47c2ed5866d1803e5d
danieldamian09 <humbertorivero9@gmail.com>
2022-06-21 16:31:58 -0300

    Feat total products in cart

--- src/components/CardItemCart.jsx
+++ src/components/CardItemCart.jsx
@@ -4,11 +4,17 @@ const CardItemCart = ({item, deleteFromCart}) => {
 	const {id, name, price, quantity} = item;
 
 	return (
-		<div className='item'>
-      <h4>{name}</h4>
-      <h5>${price},<small>00</small></h5>
-			<button onClick={() => deleteFromCart(id)}>Eliminar Uno</button>
-			<button onClick={() => deleteFromCart(id)}>Eliminar Todo</button>
+		<div className="item">
+			<h4>{name}</h4>
+			<p>Precio:
+				${price},<small>00</small>
+			</p>
+			<p>Cantidad: {quantity}</p>
+			<p style={{fontWeight:"bold"}}>Total: ${quantity * price},<small>00</small></p>
+			<div className="box-buttons">
+				<button onClick={() => deleteFromCart(id)}>Eliminar Uno</button>
+				<button onClick={() => deleteFromCart(id)}>Eliminar Todo</button>
+			</div>
 		</div>
 	);
 };
